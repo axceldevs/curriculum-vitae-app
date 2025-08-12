d0f5f25f8eb0f0b33364e802e9c9a98f3760c123
cristian bambague <axceldev98@gmail.com>
2025-08-12 12:09:39 -0500

    create home complete

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements AfterViewInit, OnDestroy {
+  @ViewChild('typedElement', { static: true }) typedElement!: ElementRef<HTMLSpanElement>;
+  private typed?: Typed;
 
+  ngAfterViewInit() {
+    this.initTyped();
+  }
+
+  initTyped() {
+    if (this.typed) {
+      this.typed.destroy();
+    }
+    this.typed = new Typed(this.typedElement.nativeElement, {
+      strings: ['Ingeniero De Sistemas', 'Backend Developer', 'Frontend Developer', 'Freelancer'],
+      typeSpeed: 80,
+      backSpeed: 50,
+      backDelay: 1500,
+      loop: true,
+      showCursor: false,
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.typed) {
+      this.typed.destroy();
+    }
+  }
 }
